d30f6f3a59233d25e993cea2915579ac2a6d44a5
sarishbiswas <sarishbiswas@gmail.com>
2025-01-02 18:34:24 +0530

    Updated readme and added comments

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Simple express demo
-
-> Express: fast, unopinionated, minimalist web framework for Node.js
+# Simple express tutorial
 
 This project shows a simple express server serving a single HTML page and using `express.static` to serve static files.
 
-Check out the [express documentation](https://expressjs.com/) for more information.
+Here I am showing the basic express enpoints with request as query param and sending response.
--- index.js
+++ index.js
@@ -4,30 +4,31 @@ const { resolve } = require('path');
 const app = express();
 const port = 3000;
 
+// Getting api endpoing as /shout?name={name}
 app.get('/shout', (req, res) => {
   res.send(req.query.name.toUpperCase());
 });
-
+// Getting api endpoing as /fullname?firstname={firstname}&lastname={lastname}
 app.get('/fullname', (req, res) => {
   res.send(req.query.firstname + ' ' + req.query.lastname);
 });
-
+// Getting api endpoing as /date?month={month}&year={year}
 app.get('/date', (req, res) => {
   res.send(req.query.month + ', ' + req.query.year);
 });
-
+// Getting api endpoing as /greet?name={name}
 app.get('/greet', (req, res) => {
   res.send('Namaste, ' + req.query.name + '!');
 });
-
+// Getting api endpoing as /address?street={street}&city={city}&state={state}
 app.get('/address', (req, res) => {
   res.send(req.query.street + ', ' + req.query.city + ', ' + req.query.state);
 });
-
+// Getting api endpoing as /email?username={username}&domain={domain}
 app.get('/email', (req, res) => {
   res.send(req.query.username + '@' + req.query.domain);
 });
-
+// It is used to make this endpoing listen to the below port, here itis 3000.
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
